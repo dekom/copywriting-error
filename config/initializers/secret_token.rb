c0ccec4f213cd734b04bc57eb19ff1d008e0b29d
Xing Zhou <dekom.dev@gmail.com>
2012-04-13 02:01:25 +0000

    Init commit with NoMethodError

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Copywriting::Application.config.secret_token = 'af01f4e9437f94a71083993191933a7b6f128f0eac54b3ed58955a8af7f2ad0858c0da8a11e45a1aef77e138dae0a017d53fee2942c1d786c56c5f0f6234ab0a'
